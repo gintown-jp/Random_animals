ddd70bc53d3784e531a9fa17d38b7809d13d7f79
gintown-jp <91199403+gintown-jp@users.noreply.github.com>
2021-12-19 12:41:06 +0900

    Initial commit
    Gitの練習のためアップロードしました。

---
+++ animalNameM.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class animalNameM : MonoBehaviour
+{
+    //動物の種類
+    string[] type = new string[]
+    {
+        "犬",
+        "猫",
+        "牛",
+        "馬"
+    };
+
+    //形容詞
+    string[] adjective = new string[]
+    {
+        "かしこい",
+        "大きい",
+        "小さい",
+        "心配性な",
+        "金持ちな",
+        "美味しい",
+        "人見知りな"
+    };
+
+
+    void Start()
+    {
+        NameSelect(adjective, type);
+    }
+
+    public void NameSelect(string[] leftText,string[] rightText)
+    {
+        string animalName = null;
+        System.Random r = new System.Random();
+
+        //乱数で配列を決定
+        var left = leftText[r.Next(0, leftText.Length)];
+        var right = rightText[r.Next(0, rightText.Length)];
+
+        //結合
+        animalName = $"{left}{right}";
+        Debug.Log(animalName);
+
+        //表示
+        this.GetComponent<Text>().text = animalName;
+    }
+
+}
